bfff7feefba097c9b2507cee583beef9580d03e3
jake seo <n00nietzsche@gmail.com>
2020-06-20 16:31:08 +0900

    Video 10/12 completed, sweep the row which is filled with colors

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,9 +3,23 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
-    console.log("updateStage worked");
+    setRowsCleared(0);
+
+    const sweepRows = newStage =>
+      newStage.reduce((acc, row) => {
+        // it means that all cells are filled with colors
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+          return acc;
+        }
+        acc.push(row);
+        return acc;
+      }, []);
+
     const updateStage = prevStage => {
       // row단위로 스테이지의 모든 cell을 뒤지면서 "clear"인지 확인
       const newStage = prevStage.map(row =>
@@ -29,6 +43,8 @@ export const useStage = (player, resetPlayer) => {
 
       if (player.collided) {
         resetPlayer();
+        // if collided it should check if we should sweep or not
+        return sweepRows(newStage);
       }
 
       return newStage;
@@ -43,5 +59,5 @@ export const useStage = (player, resetPlayer) => {
     resetPlayer,
   ]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
